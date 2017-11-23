f7fdbb24a9ad31e545bd1be57101232a9e0c18ea
Morayo Ogunsina <moraskool@gmail.com>
2017-11-23 09:58:40 -0500

    Add files via upload

---
+++ Lab1/AdderSubtracator_4bit.vhd
@@ -0,0 +1,84 @@
+----------------------------------------------------------------------------
+-- Entity:        AdderSubtractor_4bit
+-- Written By:    Morayo Ogunsina
+-- Date Created:  9/2/2015
+-- Description:    Adds or subtracts two 4 bit numbers.
+--
+-- Revision History (date, initials, description):
+-- 
+-- Dependencies:
+--		FullAdder
+--    InvertOrPass
+--    OverflowDetect
+--    RippleCarryAdder_4bit.
+---------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+
+entity AdderSubtracator_4bit is
+    Port ( A        : in  STD_LOGIC_VECTOR (3 downto 0);
+           B        : in  STD_LOGIC_VECTOR (3 downto 0);
+           SUBTRACT : in  STD_LOGIC;
+           SUM      : out STD_LOGIC_VECTOR (3 downto 0);
+           OVERFLOW : out STD_LOGIC);
+end AdderSubtracator_4bit;
+
+architecture Structural of AdderSubtracator_4bit is
+
+    -- declare the internal signals
+	signal Sum_int      : STD_LOGIC_VECTOR (3 downto 0);
+	signal Bxor_int     : STD_LOGIC_VECTOR (3 downto 0);
+	
+	-- declare the  RippleCarryAdder component
+ component RippleCarryAdder_4bit is
+     Port (A     : in   STD_LOGIC_VECTOR (3 downto 0);
+           B     : in   STD_LOGIC_VECTOR (3 downto 0);
+			  SUM   : out  STD_LOGIC_VECTOR (3 downto 0);
+           C_in  : in   STD_LOGIC;
+           C_out : out  STD_LOGIC);
+end component;
+
+    -- declare the  InvertOrPass component
+component InvertOrPass is
+	Port   ( B         : in  STD_LOGIC_VECTOR (3 downto 0);
+            BXOR      : out  STD_LOGIC_VECTOR (3 downto 0);
+            SUBTRACT  : in  STD_LOGIC);
+end component;
+
+   -- declare the  OverflowDetect component
+	component OverflowDetect is 
+	    Port ( A_MSB     : in  STD_LOGIC;
+              B_MSB     : in  STD_LOGIC;
+              SUM_MSB   : in  STD_LOGIC;
+              SUBTRACT  : in  STD_LOGIC;
+              OVERFLOW  : out STD_LOGIC);
+end component;
+
+begin
+
+    OVFL : OverflowDetect  Port Map (
+	               SUBTRACT => SUBTRACT,
+						A_MSB    => A(3),
+						B_MSB    => B(3),
+						SUM_MSB  => Sum_int(3),
+						OVERFLOW => OVERFLOW);
+						
+    RCA : RippleCarryAdder_4bit Port Map(
+                  A     => A,
+                  B     => Bxor_int,
+						C_in  => SUBTRACT,
+						C_out => OPEN,
+						SUM   => Sum_int );
+   
+	INVP : InvertOrPass Port Map  (
+	               B        => B,
+						BXOR     => Bxor_int,
+						SUBTRACT => SUBTRACT);
+						
+   
+	 
+--sum <= "1001";
+   SUM  <= Sum_int;
+end Structural;
+
---
+++ Lab1/AdderSubtractor_4bit_tb.vhd
@@ -0,0 +1,81 @@
+----------------------------------------------------------------------------
+-- Entity:        AdderSubtractor_4bit_tb
+-- Written By:    E. George Walters
+-- Date Created:  18 Aug 13
+-- Description:   VHDL testbench for RippleCarryAdder_4bit
+--
+-- Revision History (date, initials, description):
+--   26 Aug 14, egw100, Modified port signal names to reflect course standard
+-- 
+-- Dependencies:
+--   FullAdder
+----------------------------------------------------------------------------
+
+  LIBRARY ieee;
+  USE ieee.std_logic_1164.ALL;
+  USE ieee.numeric_std.ALL;
+
+----------------------------------------------------------------------------
+  ENTITY AdderSubtractor_4bit_tb IS
+  END AdderSubtractor_4bit_tb;
+----------------------------------------------------------------------------
+  architecture behavior of AdderSubtractor_4bit_tb is 
+
+  -- Unit Under Test (UUT)
+          component AdderSubtractor_4bit
+          Port ( A        : in  STD_LOGIC_VECTOR (3 downto 0);
+                 B        : in  STD_LOGIC_VECTOR (3 downto 0);
+                 SUBTRACT : in  STD_LOGIC;
+                 SUM      : out STD_LOGIC_VECTOR (3 downto 0);
+                 OVERFLOW : inout STD_LOGIC);
+          END COMPONENT;
+
+         --Inputs
+     signal A        : STD_LOGIC_VECTOR (3 downto 0):= "0000";
+     signal B        : STD_LOGIC_VECTOR (3 downto 0):= "0000";
+     signal SUBTRACT : STD_LOGIC := '0';
+      
+        --Outputs
+     signal SUM       : STD_LOGIC_VECTOR (3 downto 0);
+     signal OVERFLOW  : STD_LOGIC;		
+
+  BEGIN
+
+  -- Instantiate the Unit Under Test (UUT)
+          uut: AdderSubtractor_4bit PORT MAP(
+                  A         => A,
+                  B         => B,
+						SUBTRACT  => SUBTRACT,
+                  SUM       => SUM,
+						OVERFLOW  => OVERFLOW );
+
+ -- Stimulus process
+   stim_proc: process is
+   begin		
+      -- hold reset state for 100 ns.
+		wait for 100 ns;
+      --test for addition
+		 A        <= "1111";   --binary value for 7
+		 B        <= "1100";   --binary value for 5
+		 SUBTRACT <= '0';      --binary value for 0
+		wait for 100 ns;
+		assert (OVERFLOW = '1' --0
+		          and
+				  SUM = "1011") --12
+			report "FAILURE: OVERFLOW and/or SUM does not equal expected value." 
+			severity failure;
+		--test for subtraction
+		 A        <= "1011";   --11
+		 B        <= "0110";   --6
+		 SUBTRACT <= '1';      --SUBTRACT IS
+		wait for 100 ns;
+		assert (OVERFLOW = '0' --0
+		          and
+				  SUM = "0101") --17
+			report "FAILURE: OVERFLOW and/or SUM does not equal expected value." 
+			severity failure;
+        wait; -- will wait forever
+     END PROCESS stim_proc;
+  --  End Test Bench 
+
+  END;
